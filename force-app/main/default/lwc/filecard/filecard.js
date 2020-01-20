0b8d62c1d7bef2fd2161f1698fb052068b1233a8
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-20 13:34:55 -0600

    moved to ContentVersion via ui-api

---
+++ force-app/main/default/lwc/filecard/filecard.js
@@ -0,0 +1,84 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
+import { classSet } from 'c/classSet';
+
+import DATA from '@salesforce/schema/ContentVersion.VersionData';
+import TITLE from '@salesforce/schema/ContentVersion.Title';
+import EXTENSION from '@salesforce/schema/ContentVersion.FileExtension';
+import CD from '@salesforce/schema/ContentVersion.ContentDocumentId';
+
+export default class Filecard extends NavigationMixin(LightningElement) {
+  @api cvId = '0681F000000WNhOQAW';
+  @api showTitle;
+  @api aspectRatio;
+
+  @wire(getRecord, {
+    recordId: '$cvId',
+    fields: [DATA, TITLE, EXTENSION, CD]
+  })
+  wiredRecord;
+
+  get imgUrl() {
+    return `data:image/${this.wiredRecord.data.fields.FileExtension.value};base64,${this.wiredRecord.data.fields.VersionData.value}`;
+  }
+
+  get title() {
+    if (
+      this.showTitle &&
+      this.wiredRecord &&
+      this.wiredRecord.data &&
+      this.wiredRecord.data.fields
+    ) {
+      return `${this.wiredRecord.data.fields.Title.value}.${this.wiredRecord.data.fields.FileExtension.value}`;
+    }
+    return false;
+  }
+
+  get outerClass() {
+    return classSet('slds-file slds-file_card').add({
+      //   'slds-has-title': this.showTitle && this.computeTitle(),
+      'slds-has-title': this.showTitle && this.title,
+
+      'slds-file_loading': !this.wiredRecord.data
+    });
+  }
+
+  get hrefClass() {
+    return classSet('slds-file__crop').add({
+      'slds-file__crop_1-by-1': this.aspectRatio === '1x1',
+      'slds-file__crop_4-by-3': this.aspectRatio === '4x3',
+      'slds-file__crop_16-by-9': this.aspectRatio === '16x9'
+    });
+  }
+
+  toFile(event) {
+    event.preventDefault();
+    if (this.isCommunity()) {
+      this[NavigationMixin.Navigate]({
+        type: 'standard__recordPage',
+        attributes: {
+          objectApiName: 'ContentDocument',
+          actionName: 'view',
+          recordId: this.wiredRecord.data.fields.ContentDocumentId.value
+        }
+      });
+    } else {
+      this[NavigationMixin.Navigate]({
+        type: 'standard__namedPage',
+        attributes: {
+          pageName: 'filePreview'
+        },
+        state: {
+          //   recordIds: [this.wiredRecord.data.fields.ContentDocumentId.value],
+          selectedRecordId: this.wiredRecord.data.fields.ContentDocumentId.value
+        }
+      });
+    }
+  }
+
+  isCommunity() {
+    console.log('not a community');
+    return window.location.pathname.includes('/s/');
+  }
+}
--- force-app/main/default/lwc/relatedImages/relatedImages.js
+++ force-app/main/default/lwc/relatedImages/relatedImages.js
@@ -5,11 +5,12 @@ import getRelatedImages from '@salesforce/apex/relatedImages.getRelatedImages';
 
 export default class RelatedImages extends NavigationMixin(LightningElement) {
   @api recordId;
-  @api variant = 'grid';
+  @api variant;
   @api itemsPerRow;
   @api wrapped = false;
   @api title;
   @track modifiedImages = [];
+  @api showTitles;
 
   // get the releated urls for displaying the images
   @wire(getRelatedImages, { recordId: '$recordId' })
@@ -19,28 +20,8 @@ export default class RelatedImages extends NavigationMixin(LightningElement) {
     }
     if (data) {
       this.gotData = true;
-      this.modifiedImages = [...data].map(image => {
-        let imageUrl = `/sfc/servlet.shepherd/version/download/${
-          image.Id.split('-')[0]
-        }`;
-        if (this.isCommunity()) {
-          imageUrl = `/${this.getCommunityPath()}${imageUrl}`;
-        }
-        return {
-          ...image,
-          imageUrl
-        };
-      });
-      console.log(this.modifiedImages);
+      this.modifiedImages = data;
     }
-  }
-
-  get isCarousel() {
-    return this.variant === 'carousel' && this.modifiedImages.length > 0;
-  }
-
-  get isGrid() {
-    return this.variant === 'grid' && this.modifiedImages.length > 0;
   }
 
   get isEmpty() {
@@ -50,39 +31,5 @@ export default class RelatedImages extends NavigationMixin(LightningElement) {
   get RowStyle() {
     return `grid-template-columns: repeat(${this.itemsPerRow ||
       this.modifiedImages.length}, 1fr);`;
-  }
-
-  toFile(event) {
-    if (this.isCommunity()) {
-      this[NavigationMixin.Navigate]({
-        type: 'standard__recordPage',
-        attributes: {
-          objectApiName: 'ContentDocument',
-          actionName: 'view',
-          recordId: event.target.dataset.id
-        }
-      });
-    } else {
-      this[NavigationMixin.Navigate]({
-        type: 'standard__namedPage',
-        attributes: {
-          pageName: 'filePreview'
-        },
-        state: {
-          recordIds: this.modifiedImages
-            .map(img => img.ContentDocumentId)
-            .join(','),
-          selectedRecordId: event.target.dataset.id
-        }
-      });
-    }
-  }
-
-  isCommunity() {
-    return window.location.pathname.includes('/s/');
-  }
-
-  getCommunityPath() {
-    return window.location.pathname.split('/s/')[0].replace('/', '');
   }
 }
